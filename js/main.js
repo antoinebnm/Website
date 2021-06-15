b7ed1c635a1f1ecb1dc5f6dc91ce9c4c89fbdb77
Bonhomme <ab20040218@gmail.com>
2021-06-15 11:32:10 +0200

    all done with js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-function playsound(self) {
-    let element = document.getElementsByClassName(self);
-    let button = element.item(self).className;
-    document.getElementById('output').innerText = button;
-    
+function playsound() {
+    let valeur = "";
+    let valeur2 = "";
+    let elements = document.querySelectorAll(".button");
+    Array.from(elements).forEach((element, index) => 
+    {
+        let label = document.querySelector("div.drum label[for="+ element.id + "]");
+        valeur += "ID : " + element.id + ", Class : " + element.className + ", Label Text : " + label.innerText + ", Label Class : " + label.className + "\r";
+        if (label.className === "off") {
+            label.className = "on";
+            label.style.color = "red";
+        } else {
+            label.className = "off";
+            label.style.color = "blue";
+        }
+        valeur2 += "Label Text : " + label.innerText + ", Label Class : " + label.className  + "\r";
+    });
+    document.getElementById('output').innerText = valeur;
 }
